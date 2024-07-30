dbf705272193ffa40e8a1f5808df5c48efcfcb77
jgd666 <59641421+jgd666@users.noreply.github.com>
2024-07-31 00:06:15 +0800

    Update Hero.js
    图片最大宽度845px到2000px

--- themes/starter/components/Hero.js
+++ themes/starter/components/Hero.js
@@ -64,7 +64,7 @@ export const Hero = () => {
           {/* 产品预览图片 */}
           { siteConfig('STARTER_HERO_PREVIEW_IMAGE', null, CONFIG) && <div className="w-full px-4">
             <div
-              className="wow fadeInUp relative z-10 mx-auto max-w-[845px]"
+              className="wow fadeInUp relative z-10 mx-auto max-w-[2000px]"
               data-wow-delay=".25s"
             >
 
