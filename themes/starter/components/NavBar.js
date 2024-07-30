f821abd40b865b2bfb8d5183231edc01c1b3f88a
jgd666 <59641421+jgd666@users.noreply.github.com>
2024-07-31 01:16:22 +0800

    Update NavBar.js
    增加了  <span className="transition-transform duration-300 transform hover:scale-110">
        {siteConfig('STARTER_NAV_BUTTON_2_TEXT', null, CONFIG)}
      </span>

--- themes/starter/components/NavBar.js
+++ themes/starter/components/NavBar.js
@@ -75,10 +75,11 @@ export const NavBar = (props) => {
                             </a>
                             <a
                             href={siteConfig('STARTER_NAV_BUTTON_2_URL', null, CONFIG)}
-                            className={`signUpBtn ${buttonTextColor} rounded-md bg-[#E71926] text-[#000000] px-6 py-4 text-base font-medium duration-300 ease-in-out hover:bg-[#E71320] hover:text-[#404040] hover:scale-110`}
-                            style={{ transition: 'transform 0.3s' }}
+                            className={`signUpBtn ${buttonTextColor} rounded-md bg-[#E71926] text-[#000000] px-6 py-4 text-base font-medium duration-300 ease-in-out hover:bg-[#E71320] hover:text-[#404040] `}
                             >
-                           {siteConfig('STARTER_NAV_BUTTON_2_TEXT', null, CONFIG)}
+                              <span className="transition-transform duration-300 transform hover:scale-110">
+                                {siteConfig('STARTER_NAV_BUTTON_2_TEXT', null, CONFIG)}
+                              </span>
                             </a>
                         </div>
                     </div>
