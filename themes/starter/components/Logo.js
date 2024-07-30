febd093f009cd63f207091eaa591e46a5a980236
jgd666 <59641421+jgd666@users.noreply.github.com>
2024-07-30 23:33:41 +0800

    Update Logo.js
    logo 文字 hidden

--- themes/starter/components/Logo.js
+++ themes/starter/components/Logo.js
@@ -60,7 +60,7 @@ export const Logo = ({ white }) => {
           onClick={() => {
             router.push('/')
           }}
-          className={`${logoTextColor} dark:text-white py-1.5 header-logo-text whitespace-nowrap text-2xl font-semibold`}>
+          className={`${logoTextColor} dark:text-white py-1.5 header-logo-text whitespace-nowrap text-2xl font-semibold hidden`}>
           {siteConfig('TITLE')}
         </span>
       </div>
