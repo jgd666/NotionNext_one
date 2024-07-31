1162f8cc1e2d9190ba05fc6b0a8668bf9fd4e576
jgd666 <59641421+jgd666@users.noreply.github.com>
2024-07-31 17:01:23 +0800

    Update Blog.js
    更改文字颜色

--- themes/starter/components/Blog.js
+++ themes/starter/components/Blog.js
@@ -18,17 +18,17 @@ export const Blog = ({ posts }) => {
           <div className='-mx-4 flex flex-wrap justify-center'>
             <div className='w-full px-4'>
               <div className='mx-auto mb-[60px] max-w-[485px] text-center'>
-                <span className='mb-2 block text-lg font-semibold text-primary'>
+                <span className='mb-2 block text-lg font-semibold text-white'>
                   {siteConfig('STARTER_BLOG_TITLE', null, CONFIG)}
                 </span>
-                <h2 className='mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]'>
+                <h2 className='mb-4 text-3xl font-bold text-white dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]'>
                   {siteConfig('STARTER_BLOG_TEXT_1', null, CONFIG)}
                 </h2>
                 <p
                   dangerouslySetInnerHTML={{
                     __html: siteConfig('STARTER_BLOG_TEXT_2', null, CONFIG)
                   }}
-                  className='text-base text-body-color dark:text-dark-6'></p>
+                  className='text-base text-white dark:text-dark-6'></p>
               </div>
             </div>
           </div>
@@ -50,17 +50,17 @@ export const Blog = ({ posts }) => {
                       </Link>
                     </div>
                     <div>
-                      <span className='mb-6 inline-block rounded-[5px] bg-primary px-4 py-0.5 text-center text-xs font-medium leading-loose text-white'>
+                      <span className='mb-6 inline-block rounded-[5px] bg-[#E6BF45] px-4 py-0.5 text-center text-xs font-medium leading-loose text-white'>
                         {item.publishDay}
                       </span>
                       <h3>
                         <Link
                           href={item?.href}
-                          className='mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl'>
+                          className='mb-4 inline-block text-xl font-semibold text-white hover:text-[#E6BF45] dark:text-white dark:hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl'>
                           {item.title}
                         </Link>
                       </h3>
-                      <p className='max-w-[370px] text-base text-body-color dark:text-dark-6'>
+                      <p className='max-w-[370px] text-base text-white dark:text-dark-6'>
                         {item.summary}
                       </p>
                     </div>
